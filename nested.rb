82143216e142ea74d28d17c6f48b7e9ad722470e
Cody Fussell <cody.fussell@gmail.com>
2019-09-17 22:36:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ def adding_matz
         }
      }
 
-  programmer_hash[:yukihiro_matsumoto] = {
+  new_hash = programmer_hash[:yukihiro_matsumoto] = {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
   }.merge
