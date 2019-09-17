bd91e2ba14d4789e245807132055a218343efaa5
Cody Fussell <cody.fussell@gmail.com>
2019-09-17 22:40:12 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  return programmer_hash[:grace_hopper]
 
 end
 
@@ -37,6 +37,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+    programmer_hash[:alan_kay][:known_for] 
+     
 end
 
 def dennis_ritchies_language
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -66,6 +69,11 @@ def adding_matz
 
 	programmer_hash = 
  		{
+ 		  
+ 		    :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+ 		    },
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -80,6 +88,11 @@ def adding_matz
         }
      }
 
+  new_hash = programmer_hash[:yukihiro_matsumoto] = {
+    :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+  }.merge(programmer_hash)
+  return new_hash
     
 end
 
@@ -101,6 +114,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
      
      
 end
@@ -121,5 +136,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+     
+     programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     return programmer_hash
 
 end
